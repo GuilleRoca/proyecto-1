11673b810b5b9665296111b392603edb6839ebcb
Guillermo Rocatti <grocatti@hotmail.com>
2023-07-03 00:46:50 -0300

    se agrego POST - PUT - DELETE - CRUD

--- src/database/data.manager.js
+++ src/database/data.manager.js
@@ -3,9 +3,21 @@ const path = require('path')
 
 const ruta = path.join(__dirname, 'data.json')
 
-function leer() {
+function generarId(frutas) {
+    let mayorId = 0;
+
+    frutas.forEach((fruta) => {
+        if (Number(fruta.id) > mayorId) {
+            mayorId = Number(fruta.id);
+        }
+    });
+
+    return mayorId + 1;
+}
+
+function read() {
     return new Promise((resolve, reject) =>{
-        fs.readFile(ruta, 'utf-8', (error, result) => {   
+        fs.readFile(ruta, 'utf8', (error, result) => {   
             if(error) reject(new Error('Error. No se puede leer'))
 
             resolve(JSON.parse(result))
@@ -13,12 +25,73 @@ function leer() {
     })
 }
 
+function write(newData){
+    return new Promise((resolve, reject) =>{
+        fs.writeFile(ruta, JSON.stringify(newData , null , "\t"), "utf8", (error) => {
+            if(error) reject(new Error('Error. No se puede escribir'))
+
+            resolve(true)
+        })
+    })
+}
 
 
-
-async function findAll() {
-    const frutas = await leer()
+async function getAll() {
+    const frutas = await read()
     return frutas
 }
 
-module.exports = { findAll }
+async function getOneById(id){
+    if(!id) throw new Error('Error.El id está indefinido')
+
+    const frutas = await read()
+    const fruta = frutas.find((e) => e.id === id)
+
+    if(!fruta) throw new Error ('Error. El id ingresado no corresponde a ningúna de las frutas que tenemos en nuestra base de datos')
+
+    return fruta
+
+}
+
+async function create(fruta){
+    if(!fruta?.imagen || !fruta?.nombre || !fruta?.importe || !fruta?.stock) throw new Error(`Error. Faltan datos. Para crear una fruta se necesita imagen, nombre, importe y stock`)
+
+    let frutas = await read()
+    const frutaConInd = {id: generarId(frutas), ...fruta}
+
+    frutas.push(frutaConInd)
+    await write(frutas)
+
+    return frutaConInd
+}
+
+async function update(fruta){
+    if(!fruta?.id || !fruta?.imagen || !fruta?.nombre || !fruta?.importe || !fruta?.stock) throw new Error('Errror. Los datos están incompletos. Debe completar todos los datos para poder actualizar una fruta')
+
+    let frutas = await read()
+    const indice = frutas.findIndex((e) => e.id === fruta.id)
+
+    if (indice < 0 )throw new Error ('Error. El Id no corresponde a una fruta existente') 
+
+    frutas[indice] = fruta
+    await write(frutas)
+
+    return frutas[indice]
+}
+
+async function destroy(id){
+    if(!id) throw new Error('Error. El id está indefinido')
+
+    let frutas = await read()
+    const indice = frutas.findIndex((e) => e.id === id)
+
+    if(indice < 0) throw new Error ('Error. El id ingresado no corresponde a ningúna de las frutas que tenemos en nuestra base de datos')
+
+    const fruta = frutas[indice]
+    frutas.splice(indice,1)
+    await write(frutas)
+
+    return fruta
+}
+
+module.exports = { getOneById, getAll, create, update, destroy}
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {findAll} = require("./database/data.manager.js")
+const {getOneById, getAll, create, update, destroy} = require("./database/data.manager.js")
 
 require('dotenv').config()
 
@@ -9,11 +9,43 @@ server.use(express.json())
 server.use(express.urlencoded({ extended: true }))
 
 server.get('/frutas', (req, res) => {
-    findAll()
-        .then((coches) => res.status(200).send(coches))
+    getAll()
+        .then((frutas) => res.status(200).send(frutas))
         .catch((error) => res.status(400).send(error.message));
 })
 
+server.get('/frutas/:id' , (req,res) => {
+    const { id } = req.params
+
+    getOneById(Number(id))
+        .then((fruta) => res.status(200).send(fruta))
+        .catch((error) => res.status(400).send(error.message))
+})
+
+server.post('/frutas', (req, res) => {
+    const {imagen, nombre, importe, stock} = req.body
+
+    create({imagen, nombre, importe: Number(importe), stock: Number(stock)})
+        .then((frutas) => res.status(201).send(frutas))
+        .catch((error) => res.status(400).send(error.message));
+})
+
+server.put('/frutas/:id', (req, res) =>{
+    const {id} = req.params
+    const { imagen, nombre, importe, stock} = req.body
+
+    update({id: Number(id), imagen, nombre, importe: Number(importe), stock: Number(stock)})
+        .then((fruta) => res.status(200).send(fruta))
+        .catch((error) => res.status(400).send(error.message))
+})
+
+server.delete('/frutas/:id' , (req, res) =>{
+    const {id} = req.params
+
+    destroy(Number(id))
+        .then((fruta) => res.status(200).send(fruta))
+        .catch((error) => res.status(400).send(error.message))
+})
 
 server.use('*', (req, res) => {
     res.status(404).send(`<h1>Error 404</h1><h3>La URL indicada no existe en este servidor</h3>`);
